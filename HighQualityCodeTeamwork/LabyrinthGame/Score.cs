7d737a777f8142d60c48287848cda6570ae37d1c
m3tr0n0m3 <yanakiev_g@outlook.com>
2014-06-28 18:55:36 +0300

    Score class added.

---
+++ HighQualityCodeTeamwork/LabyrinthGame/Score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame
+{
+    public class Score
+    {
+        public void PrintScoreBoard(SortedDictionary<string, int> score)
+        {
+            foreach (KeyValuePair<string, int> p in score)
+            {
+                Console.WriteLine("Player: {0} - Score {1}", p.Key, p.Value);
+            }
+
+        }
+
+        public void AddScore(Player player)
+        {
+            SortedDictionary<string, int> score = new SortedDictionary<string, int>();
+            score.Add(player.Name, player.Points);
+
+            PrintScoreBoard(score);
+        }
+    }
+}
