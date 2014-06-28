044cf5a4ca298c6a88afd606ad7be105611e5bb0
yordan-sp <yordan_sp@hotmail.com>
2014-06-28 18:13:48 +0300

    Add some wrong class

---
+++ HighQualityCodeTeamwork/LabyrinthGame/ScoreYordan.cs
@@ -0,0 +1,14 @@
+﻿namespace LabyrinthGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ScoreYordan
+    {
+        private int t = 0;
+        System.Console.WriteLine("t");
+    }
+}
