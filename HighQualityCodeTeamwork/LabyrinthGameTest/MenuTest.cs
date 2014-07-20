f7a026b6c5e15adb1ffd387122d3fc0bda9d3a7c
m3tr0n0m3 <yanakiev_g@outlook.com>
2014-07-20 13:54:06 +0300

    Menu Tests added

--- HighQualityCodeTeamwork/LabyrinthGameTest/MenuTest.cs
+++ HighQualityCodeTeamwork/LabyrinthGameTest/MenuTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using LabyrinthGame;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LabyrinthGameTest
@@ -7,8 +9,59 @@ namespace LabyrinthGameTest
     public class MenuTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMenuGetChoiceTypeHex()
         {
+            using (StringReader sw = new StringReader("H"))
+            {
+                Console.SetIn(sw);
+
+                var testMenu = new Menu();
+                var result = testMenu.GetLabyrinthTypeFromUser();
+                Assert.AreEqual("h", result);
+
+            }
+        }
+
+        [TestMethod]
+        public void TestMenuGetChoiceTypeDiamond()
+        {
+            using (StringReader sw = new StringReader("D"))
+            {
+                Console.SetIn(sw);
+
+                var testMenu = new Menu();
+                var result = testMenu.GetLabyrinthTypeFromUser();
+                Assert.AreEqual("d", result);
+
+            }
+        }
+
+        [TestMethod]
+        public void TestMenuGetUserChoice()
+        {
+            using (StringReader sw = new StringReader("1"))
+            {
+                Console.SetIn(sw);
+
+                var testMenu = new Menu();
+                var result = testMenu.GetUserChoice();
+                Assert.AreEqual("1", result);
+
+            }
+        }
+
+        [TestMethod]
+        public void TestMenuGetUserChoiceNonValid()
+        {
+            using (StringReader sw = new StringReader("9"))
+            {
+                Console.SetIn(sw);
+
+                var testMenu = new Menu();
+                var result = testMenu.GetUserChoice();
+                Assert.Fail();
+
+            }
         }
     }
 }
