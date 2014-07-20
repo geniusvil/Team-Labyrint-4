601da784fec14020b71bb28f4fd935219a0c63de
geniusvil <violet.rost@gmail.com>
2014-07-20 15:06:29 +0300

    Player TestMethods

--- HighQualityCodeTeamwork/LabyrinthGameTest/PlayerTest.cs
+++ HighQualityCodeTeamwork/LabyrinthGameTest/PlayerTest.cs
@@ -1,14 +1,42 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
 
 namespace LabyrinthGameTest
 {
+    using System;
+    using LabyrinthGame;
+    using LabyrinthGame.Interfaces;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
     public class PlayerTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static IPlayer player;
+
+        [ClassInitialize]
+        public static void PlayerClassInitialize(TestContext testContext)
         {
+            player = new Player();
+
+        }
+
+        [TestMethod]
+        public void UpdatePointsTrue()
+        {
+            Assert.IsTrue(player.Points == 0,"Player initial points are not 0");
+            player.UpdatePoints();
+            Assert.IsTrue(player.Points== 1,"After first update player's points are not 1");
+        }
+
+        [TestMethod]
+        public void UpdatePosition1x0RowChangedTest()
+        {
+            ICoordinate newCoodrinate = new Coordinate(1,0);
+            IPlayer initializedPlayer = new Player(); 
+           
+            player.UpdatePosition(newCoodrinate);
+            Assert.IsTrue(player.Coordinates.Row == (initializedPlayer.Coordinates.Row + 1), "Row is not changed with 1");
+            Assert.IsTrue(player.Coordinates.Col == (initializedPlayer.Coordinates.Col), "Col is changed");
+
         }
     }
 }
