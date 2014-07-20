1e2eeab21d0fc856ec9b3a621943ff6600322f34
Dimitar Mitev <dnmitev@gmail.com>
2014-07-20 11:58:50 +0300

    Namespace bug fixed

--- HighQualityCodeTeamwork/LabyrinthGameTest/UnitTest1.cs
+++ HighQualityCodeTeamwork/LabyrinthGameTest/UnitTest1.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace LabyrinthGameTest
+﻿namespace LabyrinthGameTest
 {
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
     public class UnitTest1
     {
