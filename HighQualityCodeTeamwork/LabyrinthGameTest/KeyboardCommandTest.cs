215f238428d7905c1ed186817a6bbec8546a136f
smin4ev <s_min4ev@abv.bg>
2014-07-20 14:14:50 +0300

    Class Initialice at KeyBoardCommnd.cs

--- HighQualityCodeTeamwork/LabyrinthGameTest/KeyboardCommandTest.cs
+++ HighQualityCodeTeamwork/LabyrinthGameTest/KeyboardCommandTest.cs
@@ -10,20 +10,28 @@ namespace LabyrinthGameTest
     [TestClass]
     public class KeyboardCommandTest
     {
+        static IUserCommand userCommand;
+        [ClassInitialize]
+        public static void UserCommandClassInicialize(TestContext testContext)
+        { 
+            userCommand = new KeyboardCommand();    
+        }
+
           [TestMethod]
         public void MoveLeftTest_True()
         {
-            IUserCommand userCommand = new KeyboardCommand();
+           
             ICoordinate coordinate = userCommand.MoveLeft();
             Assert.IsTrue(coordinate.Row ==0,"Row is not 0");
             Assert.IsTrue(coordinate.Col == -1, "Col is not -1");
 
         }
 
+
+
              [TestMethod]
           public void MoveLeftTest_False()
           {
-              IUserCommand userCommand = new KeyboardCommand();
               ICoordinate coordinate = userCommand.MoveLeft();
               Assert.IsFalse(coordinate.Row != 0, "Row is not 0");
               Assert.IsFalse(coordinate.Col != -1, "Col is not -1");
